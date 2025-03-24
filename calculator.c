006b63451e68a8a7a43acb98d0ceb52baeed78b5
sam-sanikop <sanikopsamarth7@gmail.com>
2025-03-24 09:15:51 +0530

    4 operations added

---
+++ calculator.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int sum,diff,mul,div;
+    int a,b;
+    a=10;
+    b=20;
+    sum=a+b;
+    diff=a-b;
+    mul=a*b;
+    if (b!=0)
+    {
+        div=a/b
+    }
+}
